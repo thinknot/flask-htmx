70c013f8517f581fe49cab4c992b8ad2c022b97d
AnesuMurakata <murac96@gmail.com>
2022-03-17 12:18:10 +0200

    Readded persistent storage for online

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app import create_db
 import os
-#persistent_path = os.getenv("PERSISTENT_STORAGE_DIR")
+persistent_path = os.getenv("PERSISTENT_STORAGE_DIR")
 
 app = Flask(__name__)
 
-#db_path = os.path.join(persistent_path, "sqlite.db")
+db_path = os.path.join(persistent_path, "sqlite.db")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///sqlite.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{db_path}'
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
